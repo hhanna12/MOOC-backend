42a7765c6e29c628fac369eddfc489d543becac2
hannarii <hannarii@uoregon.edu>
2020-11-24 16:13:30 +0200

    fixed some tasks

--- mongo.js
+++ mongo.js
@@ -1,9 +1,8 @@
-const process = ''
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -14,13 +13,13 @@ const url =
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
-if (process.argv.length === 3) {
+if (process.argv.length == 3) {
     console.log('phonebook: ')
     Person.find({}).then(result => {
         result.forEach(person => {
@@ -30,13 +29,13 @@ if (process.argv.length === 3) {
     })
 }
 
-if (process.argv.length === 5){
+if (process.argv.length == 5){
     const person = new Person({
         name: process.argv[3],
         number: process.argv[4]
-    })
+      })
     person.save().then(response => {
-        console.log('added ', person.name, ' number ', person.number, ' to phonebook')
+   console.log('added ', person.name, ' number ', person.number, ' to phonebook')
         mongoose.connection.close()
-    })
+      })
 }
