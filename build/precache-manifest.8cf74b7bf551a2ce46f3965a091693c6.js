67986fb91d213da11e522ef4f6b3537c0c7c6cd2
hannarii <hannarii@uoregon.edu>
2020-11-24 15:49:25 +0200

    updated frontend heroku

---
+++ build/precache-manifest.8cf74b7bf551a2ce46f3965a091693c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95719126e0b3971397ce032bd41744eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d86a077079fc0535f50",
+    "url": "/static/css/main.0b300a9e.chunk.css"
+  },
+  {
+    "revision": "740e955edf9b73a83760",
+    "url": "/static/js/2.584d881a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.584d881a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d86a077079fc0535f50",
+    "url": "/static/js/main.9c8975ab.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
